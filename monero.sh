3890c2338ddb3dffaac1da9af90a57f804de0919
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2021-05-13 00:57:30 -0300

    Add files via upload

---
+++ monero.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --disable-gpu --algorithm randomx --pool ca.monero.herominers.com:10190 --wallet 87xs58NhvvdDhwjRyDoiEGQZ5yqGtoSqqWZU12MBKgUbJPQ6vmx6obM4arqoCqvz94aNrGEL6AsQYHvTqdE7LTdTFEqr49p
+
+
+
+
+
+
